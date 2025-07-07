b7c5082528fc3aa8caba8432656f38544c1be14e
Oleksandr <lowka2018@gmail.com>
2025-07-08 00:33:41 +0200

    add react keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [pressedKey, setPressedKey] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleKey = (event: KeyboardEvent) => {
+      setPressedKey(event.key);
+    };
+
+    document.addEventListener('keyup', handleKey);
+
+    return () => {
+      document.removeEventListener('keyup', handleKey);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <p className="App__message">
+        {pressedKey
+          ? `The last pressed key is [${pressedKey}]`
+          : `Nothing was pressed yet`}
+      </p>
+    </div>
+  );
+};
